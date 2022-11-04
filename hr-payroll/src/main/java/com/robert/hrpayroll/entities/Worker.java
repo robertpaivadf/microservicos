8d30d251373ad456d2b604314f0a4ce3f4122c8e
Robert P Pereira <robertpaiva.df@gmail.com>
2022-11-04 19:01:39 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/com/robert/hrpayroll/entities/Worker.java
@@ -0,0 +1,64 @@
+package com.robert.hrpayroll.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Worker implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private long id;
+	private String name;
+	private Double dailyIncome;
+
+	public Worker() {
+	}
+
+	public Worker(long id, String name, Double dailyIncome) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dailyIncome = dailyIncome;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getDailyIncome() {
+		return dailyIncome;
+	}
+
+	public void setDailyIncome(Double dailyIncome) {
+		this.dailyIncome = dailyIncome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Worker other = (Worker) obj;
+		return id == other.id;
+	}
+
+}
