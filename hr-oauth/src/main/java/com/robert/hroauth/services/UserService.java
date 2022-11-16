1c7e74973c9784688ebf0501816e47999158c3c7
Robert P Pereira <robertpaiva.df@gmail.com>
2022-11-16 19:44:24 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/robert/hroauth/services/UserService.java
@@ -0,0 +1,32 @@
+package com.robert.hroauth.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.robert.hroauth.entities.User;
+import com.robert.hroauth.feignclients.UserFeignClient;
+
+@Service
+public class UserService {
+
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+	
+	
+	@Autowired
+	private UserFeignClient userFeignClient;
+	
+	public User findByEmail(String email) {
+		User user = userFeignClient.findByEmail2(email).getBody();
+		if(user == null) {
+			logger.error("Email not found: " + email);
+			throw new IllegalArgumentException("Email not found");
+		}
+		logger.error("Email found: " + email);
+		return user;
+	}
+	
+	
+	
+}
