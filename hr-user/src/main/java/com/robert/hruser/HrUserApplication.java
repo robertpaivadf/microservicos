358869af3fb985b0a80d13d98504a0dc65d4d77e
Robert P Pereira <robertpaiva.df@gmail.com>
2022-11-16 16:48:04 -0300

    Database seeding and using BCRypt passoword encoder

--- hr-user/src/main/java/com/robert/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/robert/hruser/HrUserApplication.java
@@ -1,15 +1,28 @@
 package com.robert.hruser;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication implements CommandLineRunner{
 
+	//USANDO O BCRYPT PARA CRIPTOGRAFAR SENHAS
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder; //não está instanciado por padrão no Sprig, nesse caso precisa gerar um BEAN na classe AppConfig
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("BCRYPT (123456) = " + passwordEncoder.encode("123456"));		
 	}
 
 }
