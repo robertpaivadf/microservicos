05fcfce6c9cbb17b8eaf278ef9c538059843f313
Robert P Pereira <robertpaiva.df@gmail.com>
2022-11-16 18:50:58 -0300

    UserRepository, UserSesource, Zull Config em using RequestPararm parameter

---
+++ hr-user/src/main/java/com/robert/hruser/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.robert.hruser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.robert.hruser.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	User findByEmail(String email);
+}
